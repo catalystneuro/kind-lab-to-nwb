145bed9cb70607ee49837f98285af8d727907c5f
alessandratrapani <alessandramaria.trapani@gmail.com>
2025-02-10 15:43:50 +0100

    refactor: enhance session_to_nwb function and update metadata structure

--- src/kind_lab_to_nwb/arc_ecephys_2024/convert_session.py
+++ src/kind_lab_to_nwb/arc_ecephys_2024/convert_session.py
@@ -5,17 +5,22 @@ from pathlib import Path
 from typing import Union
 from zoneinfo import ZoneInfo
 
-from kind_lab_to_nwb.arc_ecephys_2024 import ArcEcephys2024NWBConverter
 from neuroconv.utils import (
     dict_deep_update,
     load_dict_from_file,
 )
+from neuroconv.tools.path_expansion import LocalPathExpander
+
+from nwbconverter import ArcEcephys2024NWBConverter
+import pandas as pd
 
 
 def session_to_nwb(
     data_dir_path: Union[str, Path],
     output_dir_path: Union[str, Path],
+    path_expander_metadata: dict,
     stub_test: bool = False,
+    overwrite: bool = False,
 ):
 
     data_dir_path = Path(data_dir_path)
@@ -26,64 +31,96 @@ def session_to_nwb(
         parents=True,
         exist_ok=True,
     )
-
-    session_id = "subject_identifier_usually"
-    nwbfile_path = output_dir_path / f"{session_id}.nwb"
+    subject_id = path_expander_metadata["metadata"]["Subject"]["subject_id"]
+    session_id = path_expander_metadata["metadata"]["NWBFile"]["session_id"]
+    nwbfile_path = output_dir_path / f"sub_{subject_id}-ses{session_id}.nwb"
 
     source_data = dict()
     conversion_options = dict()
 
     # Add Recording
-    source_data.update(dict(Recording=dict()))
-    conversion_options.update(dict(Recording=dict(stub_test=stub_test)))
+    recordings_folder_path = path_expander_metadata["source_data"]["OpenEphysRecording"]["folder_path"]
+    source_data.update(dict(OpenEphysRecording=dict(folder_path=recordings_folder_path, stream_name="Signals CH")))
+    conversion_options.update(dict(OpenEphysRecording=dict(stub_test=stub_test)))
 
-    # Add Sorting
-    source_data.update(dict(Sorting=dict()))
-    conversion_options.update(dict(Sorting=dict()))
+    # Add Video
+    video_file_path = next(data_dir_path.glob(f"{subject_id}/{session_id}/*.avi"))
+    source_data.update(dict(Video=dict(file_paths=[video_file_path])))
+    conversion_options.update(dict(Video=dict(stub_test=stub_test)))
 
-    # Add Behavior
-    source_data.update(dict(Behavior=dict()))
-    conversion_options.update(dict(Behavior=dict()))
-
+    # Instantiate converter
     converter = ArcEcephys2024NWBConverter(source_data=source_data)
 
     # Add datetime to conversion
     metadata = converter.get_metadata()
-    date = datetime.datetime(
-        year=2020,
-        month=1,
-        day=1,
-        tzinfo=ZoneInfo("US/Eastern"),
-    )
-    metadata["NWBFile"]["session_start_time"] = date
+    session_date = path_expander_metadata["metadata"]["extras"]["session_date"]
+    session_time = path_expander_metadata["metadata"]["extras"]["session_time"]
+    session_start_time = datetime.datetime.strptime(f"{session_date} {session_time}", "%Y-%m-%d %H-%M-%S")
+    metadata["NWBFile"]["session_start_time"] = session_start_time.replace(tzinfo=ZoneInfo("Europe/London"))
 
     # Update default metadata with the editable in the corresponding yaml file
-    editable_metadata_path = Path(__file__).parent / "arc_ecephys_2024_metadata.yaml"
+    editable_metadata_path = Path(__file__).parent / "metadata.yaml"
     editable_metadata = load_dict_from_file(editable_metadata_path)
     metadata = dict_deep_update(
         metadata,
         editable_metadata,
     )
+    metadata["NWBFile"]["session_description"] = editable_metadata["SessionType"][session_id]["session_description"]
 
-    metadata["Subject"]["subject_id"] = "a_subject_id"  # Modify here or in the yaml file
+    # Update default metadata with the metadata extracted from the path expander
+    metadata = dict_deep_update(
+        metadata,
+        path_expander_metadata["metadata"],
+    )
+
+    # Update subject genotype
+    file_path = data_dir_path / "channels_details_v2.xlsx"
+    df = pd.read_excel(file_path)
+    subject_genotype = df[df["Folder"] == subject_id]["Genotype"].iloc[0]
+    if subject_genotype == "wt":
+        subject_genotype = "WT"
+    if subject_genotype == "het":
+        subject_genotype = "Syngap+/∆-GAP"
+    else:
+        raise ValueError(f"Genotype {subject_genotype} not recognized")
+
+    metadata["Subject"]["genotype"] = subject_genotype
 
     # Run conversion
     converter.run_conversion(
         metadata=metadata,
         nwbfile_path=nwbfile_path,
         conversion_options=conversion_options,
+        overwrite=overwrite,
     )
 
 
 if __name__ == "__main__":
 
     # Parameters for conversion
-    data_dir_path = Path("/Directory/With/Raw/Formats/")
-    output_dir_path = Path("~/conversion_nwb/")
-    stub_test = False
+    data_dir_path = Path("D:/Kind-CN-data-share/neuronal_circuits/fear_conditionning_paradigm")
+    output_dir_path = Path("D:/kind_lab_conversion_nwb")
+
+    source_data_spec = {
+        "OpenEphysRecording": {
+            "base_directory": data_dir_path,
+            "folder_path": "{subject_id}/{session_id}/{subject_id}_{session_date}_{session_time}_{task}",
+        },
+    }
+
+    # Instantiate LocalPathExpander
+    path_expander = LocalPathExpander()
+
+    # Expand paths and extract metadata
+    metadata_list = path_expander.expand_paths(source_data_spec)
+
+    stub_test = True
+    overwrite = True
 
     session_to_nwb(
         data_dir_path=data_dir_path,
         output_dir_path=output_dir_path,
+        path_expander_metadata=metadata_list[140],
         stub_test=stub_test,
+        overwrite=overwrite,
     )
--- src/kind_lab_to_nwb/arc_ecephys_2024/nwbconverter.py
+++ src/kind_lab_to_nwb/arc_ecephys_2024/nwbconverter.py
@@ -3,21 +3,13 @@
 from neuroconv import (
     NWBConverter,
 )
-from neuroconv.datainterfaces import (
-    PhySortingInterface,
-    SpikeGLXRecordingInterface,
-)
-
-from .arc_ecephys_2024 import (
-    ArcEcephys2024BehaviorInterface,
-)
+from neuroconv.datainterfaces import OpenEphysRecordingInterface, VideoInterface
 
 
 class ArcEcephys2024NWBConverter(NWBConverter):
     """Primary conversion class for my extracellular electrophysiology dataset."""
 
     data_interface_classes = dict(
-        Recording=SpikeGLXRecordingInterface,
-        Sorting=PhySortingInterface,
-        Behavior=ArcEcephys2024BehaviorInterface,
+        OpenEphysRecording=OpenEphysRecordingInterface,
+        Video=VideoInterface,
     )

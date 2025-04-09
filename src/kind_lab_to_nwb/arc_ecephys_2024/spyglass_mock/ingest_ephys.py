d700505b4988cd39fbf3cee300f73671cb07f603
Alessandra Trapani <alessandramaria.trapani@gmail.com>
2025-04-09 10:19:14 +0200

    feat: add test_ephys function to validate raw data ingestion from NWB file

--- src/kind_lab_to_nwb/arc_ecephys_2024/spyglass_mock/ingest_ephys.py
+++ src/kind_lab_to_nwb/arc_ecephys_2024/spyglass_mock/ingest_ephys.py
@@ -1,8 +1,9 @@
 """Ingest mock ephys data from an NWB file into a spyglass database."""
 
-import os
 import datajoint as dj
 from pathlib import Path
+import numpy as np
+from pynwb import NWBHDF5IO
 
 dj.conn(use_tls=False)
 
@@ -23,6 +24,17 @@ from spyglass.spikesorting.analysis.v1.group import SortedSpikesGroup
 from spyglass.utils.nwb_helper_fn import get_nwb_copy_filename
 
 
+def test_ephys(nwbfile_path: Path):
+    nwb_copy_file_name = get_nwb_copy_filename(nwbfile_path.name)
+    electrical_series = (sgc.Raw & {"nwb_file_name": nwb_copy_file_name}).fetch_nwb()[0]["raw"]
+    spyglass_raw_data = np.asarray(electrical_series.data[:10])
+    with NWBHDF5IO(nwbfile_path, "r") as io:
+        nwbfile = io.read()
+        nwb_raw_data = np.asarray(nwbfile.acquisition["electrical_series"].data[:10])
+    np.testing.assert_array_equal(spyglass_raw_data, nwb_raw_data)
+    print("Test passed: Raw data matches between Spyglass and NWB file.")
+
+
 def main():
     nwbfile_path = Path("/media/alessandra/HD2/kind_lab_conversion_nwb/Spyglass/raw/mock_ephys.nwb")
     nwb_copy_file_name = get_nwb_copy_filename(nwbfile_path.name)
@@ -31,8 +43,8 @@ def main():
         (sgc.Session & {"nwb_file_name": nwb_copy_file_name}).delete()
     if sgc.Nwbfile & {"nwb_file_name": nwb_copy_file_name}:
         (sgc.Nwbfile & {"nwb_file_name": nwb_copy_file_name}).delete()
-    if sgc.ProbeType & {"probe_type": "my_probe_type"}:
-        (sgc.ProbeType & {"probe_type": "my_probe_type"}).delete()
+    if sgc.ProbeType & {"probe_type": "probe_type"}:
+        (sgc.ProbeType & {"probe_type": "probe_type"}).delete()
 
     sgi.insert_sessions(str(nwbfile_path), rollback_on_fail=True, raise_err=True)
     print("=== Session ===")
@@ -44,13 +56,15 @@ def main():
     print("=== Electrode Group ===")
     print(sgc.ElectrodeGroup & {"nwb_file_name": nwb_copy_file_name})
     print("=== Probe ===")
-    print(sgc.Probe & {"probe_type": "my_probe_type"})
+    print(sgc.Probe & {"probe_type": "probe_type"})
     print("=== Probe Shank ===")
-    print(sgc.Probe.Shank & {"probe_type": "my_probe_type"})
+    print(sgc.Probe.Shank & {"probe_type": "probe_type"})
     print("=== Probe Electrode ===")
-    print(sgc.Probe.Electrode & {"probe_type": "my_probe_type"})
+    print(sgc.Probe.Electrode & {"probe_type": "probe_type"})
     print("=== Raw ===")
     print(sgc.Raw & {"nwb_file_name": nwb_copy_file_name})
+
+    test_ephys(nwbfile_path=nwbfile_path)
 
 
 if __name__ == "__main__":
--- src/kind_lab_to_nwb/arc_ecephys_2024/spyglass_mock/mock_ephys_nwbfile.py
+++ src/kind_lab_to_nwb/arc_ecephys_2024/spyglass_mock/mock_ephys_nwbfile.py
@@ -2,39 +2,26 @@
 
 from pynwb.testing.mock.file import mock_NWBFile
 from pynwb.testing.mock.ecephys import mock_ElectricalSeries
-from ndx_franklab_novela import DataAcqDevice, CameraDevice, Probe, Shank, ShanksElectrode, NwbElectrodeGroup
+from ndx_franklab_novela import DataAcqDevice, Probe, Shank, ShanksElectrode, NwbElectrodeGroup
 from pynwb import NWBHDF5IO
 import numpy as np
 from pathlib import Path
 
 
-def main():
-    nwbfile = mock_NWBFile(
-        identifier="my_identifier", session_description="my_session_description", session_id="my_session_id"
-    )
-
-    data_acq_device = DataAcqDevice(
-        name="my_data_acq", system="my_system", amplifier="my_amplifier", adc_circuit="my_adc_circuit"
-    )
-    camera_device = CameraDevice(
-        name="Camera 1",
-        meters_per_pixel=1.0,
-        model="my_model",
-        lens="my_lens",
-        camera_name="my_camera_name",
-    )
+def add_ephys(nwbfile):
+    data_acq_device = DataAcqDevice(name="data_acq", system="system", amplifier="amplifier", adc_circuit="adc_circuit")
     nwbfile.add_device(data_acq_device)
-    nwbfile.add_device(camera_device)
 
-    electrode = ShanksElectrode(name="1", rel_x=0.0, rel_y=0.0, rel_z=0.0)
+    electrode = ShanksElectrode(name="0", rel_x=0.0, rel_y=0.0, rel_z=0.0)
     shanks_electrodes = [electrode]
-    shank = Shank(name="1", shanks_electrodes=shanks_electrodes)
+    shank = Shank(name="0", shanks_electrodes=shanks_electrodes)
     probe = Probe(
-        name="my_probe",
+        name="probe",
         id=0,
-        probe_type="my_probe_type",
-        units="my_units",
-        probe_description="my_probe_description",
+        probe_type="probe_type",
+        units="units",
+        description="description",
+        probe_description="0",
         contact_side_numbering=False,
         contact_size=1.0,
         shanks=[shank],
@@ -43,11 +30,11 @@ def main():
 
     # add to electrical series
     electrode_group = NwbElectrodeGroup(
-        name="my_electrode_group",
-        description="my_description",
-        location="my_location",
+        name="electrode_group",
+        description="group_description",
+        location="location",
         device=probe,
-        targeted_location="my_targeted_location",
+        targeted_location="targeted_location",
         targeted_x=0.0,
         targeted_y=0.0,
         targeted_z=0.0,
@@ -63,10 +50,10 @@ def main():
     for col in extra_cols:
         nwbfile.add_electrode_column(name=col, description=f"description for {col}")
     nwbfile.add_electrode(
-        location="my_location",
+        location="location",
         group=electrode_group,
-        probe_shank=1,
-        probe_electrode=1,
+        probe_shank=0,
+        probe_electrode=0,
         bad_channel=False,
         ref_elect_id=0,
         x=0.0,
@@ -74,8 +61,18 @@ def main():
         z=0.0,
     )
     electrodes = nwbfile.electrodes.create_region(name="electrodes", region=[0], description="electrodes")
-    mock_ElectricalSeries(electrodes=electrodes, nwbfile=nwbfile, data=np.ones((10, 1)))
+    mock_ElectricalSeries(
+        name="electrical_series",
+        electrodes=electrodes,
+        nwbfile=nwbfile,
+        timestamps=np.arange(20),
+        data=np.ones((20, 1)),
+    )
 
+
+if __name__ == "__main__":
+    nwbfile = mock_NWBFile(identifier="identifier", session_description="session_description")
+    add_ephys(nwbfile)
     # add processing module to make spyglass happy
     nwbfile.create_processing_module(name="behavior", description="dummy behavior module")
 
@@ -84,8 +81,4 @@ def main():
         nwbfile_path.unlink()
     with NWBHDF5IO(nwbfile_path, "w") as io:
         io.write(nwbfile)
-    print(f"mock ephys NWB file successfully written at {nwbfile_path}")
-
-
-if __name__ == "__main__":
-    main()
+    print(f"mock video NWB file successfully written at {nwbfile_path}")

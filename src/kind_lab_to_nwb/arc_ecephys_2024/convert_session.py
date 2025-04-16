77c5dcc7f1058cfc573d08453fedd4d188863fa6
alessandratrapani <alessandramaria.trapani@gmail.com>
2025-04-16 14:21:09 +0200

    feat: update data directory paths and enhance session processing with error handling

--- src/kind_lab_to_nwb/arc_ecephys_2024/convert_session.py
+++ src/kind_lab_to_nwb/arc_ecephys_2024/convert_session.py
@@ -26,6 +26,7 @@ from spyglass_utils import (
     add_behavioral_events,
 )
 import pandas as pd
+from tqdm import tqdm
 
 
 def session_to_nwb(
@@ -140,9 +141,9 @@ if __name__ == "__main__":
 
     # Parameters for conversion
     # data_dir_path = Path("/media/alessandra/HD2/Kind-CN-data-share/neuronal_circuits/fear_conditionning_paradigm")
-    data_dir_path = Path("E:/Kind-CN-data-share/neuronal_circuits/fear_conditionning_paradigm")
+    data_dir_path = Path("D:/Kind-CN-data-share/neuronal_circuits/fear_conditionning_paradigm")
     # output_dir_path = Path("/media/alessandra/HD2/kind_lab_conversion_nwb")
-    output_dir_path = Path("E:/kind_lab_conversion_nwb")
+    output_dir_path = Path("D:/kind_lab_conversion_nwb")
 
     source_data_spec = {
         "OpenEphysRecording": {
@@ -159,11 +160,15 @@ if __name__ == "__main__":
 
     stub_test = False
     overwrite = True
-    for metadata in metadata_list[:5]:
-        session_to_nwb(
-            data_dir_path=data_dir_path,
-            output_dir_path=output_dir_path,
-            path_expander_metadata=metadata,
-            stub_test=stub_test,
-            overwrite=overwrite,
-        )
+
+    for metadata in tqdm(metadata_list, desc="Processing sessions"):
+        try:
+            session_to_nwb(
+                data_dir_path=data_dir_path,
+                output_dir_path=output_dir_path,
+                path_expander_metadata=metadata,
+                stub_test=stub_test,
+                overwrite=overwrite,
+            )
+        except Exception as e:
+            print(f"Error processing {metadata['source_data']['OpenEphysRecording']['folder_path']}: {e}")

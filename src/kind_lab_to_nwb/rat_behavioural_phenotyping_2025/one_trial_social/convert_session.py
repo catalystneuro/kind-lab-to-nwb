9a55f8a1a64132cad904f9dbb6342fcf000be634
alessandratrapani <alessandramaria.trapani@gmail.com>
2025-07-30 15:44:54 +0200

    Refactor video and audio interface handling in session conversion script; update metadata for behavioral videos

--- src/kind_lab_to_nwb/rat_behavioural_phenotyping_2025/one_trial_social/convert_session.py
+++ src/kind_lab_to_nwb/rat_behavioural_phenotyping_2025/one_trial_social/convert_session.py
@@ -11,11 +11,16 @@ from neuroconv.utils import (
     dict_deep_update,
     load_dict_from_file,
 )
+from neuroconv.datainterfaces import AudioInterface
 
 from kind_lab_to_nwb.rat_behavioural_phenotyping_2025.one_trial_social.nwbconverter import (
     OneTrialSocialNWBConverter,
 )
-from kind_lab_to_nwb.rat_behavioural_phenotyping_2025.interfaces import get_observation_ids
+from kind_lab_to_nwb.rat_behavioural_phenotyping_2025.interfaces import (
+    BORISBehavioralEventsInterface,
+    SpyglassVideoInterface,
+    get_observation_ids,
+)
 from kind_lab_to_nwb.rat_behavioural_phenotyping_2025.utils import (
     extract_subject_metadata_from_excel,
     get_subject_metadata_from_task,
@@ -48,7 +53,6 @@ def session_to_nwb(
     subject_id = f"{subject_metadata['animal ID']}_{subject_metadata['cohort ID']}"
     nwbfile_path = output_dir_path / f"sub-{subject_id}_ses-{session_id}.nwb"
 
-    source_data = dict()
     conversion_options = dict()
 
     editable_metadata_path = Path(__file__).parent / "metadata.yaml"
@@ -56,12 +60,32 @@ def session_to_nwb(
     task_metadata = editable_metadata["SessionTypes"][session_id]
 
     # Add Behavioral Video
+    data_interfaces = []
+    video_starting_times = []
     if len(video_file_paths) == 1:
         file_paths = convert_ts_to_mp4(video_file_paths)
-        source_data.update(dict(Video=dict(file_paths=file_paths, video_name="BehavioralVideo")))
-        conversion_options.update(dict(Video=dict(task_metadata=task_metadata)))
+        video_interface = SpyglassVideoInterface(file_paths=file_paths, video_name="BehavioralVideo")
+        data_interfaces.append(video_interface)
+        conversion_options.update(dict(SpyglassVideoInterface=dict(task_metadata=task_metadata)))
     elif len(video_file_paths) > 1:
-        raise ValueError(f"Multiple video files found for {subject_id}.")
+        if "HabD1" in session_id:
+            for i, video_file_path in enumerate(video_file_paths):
+                file_paths = convert_ts_to_mp4([video_file_path])
+                video_name = f"BehavioralVideoHabD1Trial{i+1}"
+                video_interface = SpyglassVideoInterface(file_paths=file_paths, video_name=video_name)
+                video_file_name = Path(video_file_path).stem
+                datetime_from_filename = parse_datetime_from_filename(video_file_name)
+                starting_time = (datetime_from_filename - session_start_time).total_seconds()
+                video_starting_times.append(starting_time)
+                video_interface._starting_time = starting_time
+                data_interfaces.append(video_interface)
+                test_task_metadata = task_metadata.copy()
+                test_task_metadata["name"] = task_metadata["name"] + "_trial" + str(i + 1)
+                test_task_metadata["task_epochs"] = [i + 1]
+                conversion_options.update({f"SpyglassVideoInterface00{i+1}": dict(task_metadata=test_task_metadata)})
+
+        else:
+            raise ValueError(f"Multiple video files found for {subject_id}.")
 
     # Add One Trial Social Annotated events from BORIS output
     if boris_file_path is not None and "Test" in session_id:
@@ -71,15 +95,16 @@ def session_to_nwb(
         )
         if observation_id is None:
             raise ValueError(f"No observation ID found containing subject ID '{subject_id}'")
-        source_data.update(dict(OneTrialSocialBehavior=dict(file_path=boris_file_path, observation_id=observation_id)))
-        conversion_options.update(dict(OneTrialSocialBehavior=dict()))
+        boris_interface = BORISBehavioralEventsInterface(file_path=boris_file_path, observation_id=observation_id)
+        data_interfaces.append(boris_interface)
 
     # Add Audio if available
     if audio_file_path is not None:
-        source_data.update(dict(Audio=dict(file_paths=[audio_file_path])))
-        conversion_options.update(dict(Audio=dict(stub_test=stub_test, write_as="acquisition")))
+        audio_interface = AudioInterface(file_paths=audio_file_path)
+        data_interfaces.append(audio_interface)
+        conversion_options.update(dict(AudioInterface=dict(stub_test=stub_test, write_as="acquisition")))
 
-    converter = OneTrialSocialNWBConverter(source_data=source_data)
+    converter = OneTrialSocialNWBConverter(data_interfaces=data_interfaces, verbose=True)
 
     # Add datetime to conversion
     metadata = converter.get_metadata()
@@ -134,14 +159,14 @@ if __name__ == "__main__":
     # Parameters for conversion
     data_dir_path = Path("D:/Kind-CN-data-share/behavioural_pipeline/1 Trial Social")
     output_dir_path = Path("D:/kind_lab_conversion_nwb/behavioural_pipeline/1_trial_social")
-    subjects_metadata_file_path = Path("D:/Kind-CN-data-share/behavioural_pipeline/RAT ID metadata Yunkai.xlsx")
+    subjects_metadata_file_path = Path("D:/Kind-CN-data-share/behavioural_pipeline/general_metadata.xlsx")
     task_acronym = "1TS"
     session_ids = get_session_ids_from_excel(subjects_metadata_file_path, task_acronym)
 
     subjects_metadata = extract_subject_metadata_from_excel(subjects_metadata_file_path)
     subjects_metadata = get_subject_metadata_from_task(subjects_metadata, task_acronym)
 
-    session_id = session_ids[-1]  # Test
+    session_id = session_ids[0]  # Test
     subject_metadata = subjects_metadata[52]  # subject 635Arid1b(11)
 
     cohort_folder_path = data_dir_path / subject_metadata["line"] / f"{subject_metadata['cohort ID']}_{task_acronym}"
@@ -159,13 +184,18 @@ if __name__ == "__main__":
     video_folder_path = cohort_folder_path / session_id
     if not video_folder_path.exists():
         raise FileNotFoundError(f"Folder {cohort_folder_path} does not exist")
-    if session_id == "HabD2" or session_id == "Test":
-        video_file_paths = list(video_folder_path.glob(f"*{subject_metadata['animal ID']}*"))
-    if session_id == "HabD1":
-        video_file_paths = list(video_folder_path.glob(f"**"))  # TODO add video name pattern
+    video_file_paths = list(video_folder_path.glob(f"*{subject_metadata['animal ID']}*"))
+    if len(video_file_paths) == 0:
+        cage_id = subject_metadata["cage ID"]
+        if not np.isnan(cage_id):
+            cage_id = int(cage_id)
+            video_file_paths = list(video_folder_path.glob(f"*cage{cage_id}*"))
 
-    video_path = Path(video_file_paths[0])
-    session_start_time = parse_datetime_from_filename(video_path.name)
+    session_start_times = []
+    for video_file_path in video_file_paths:
+        session_start_times.append(parse_datetime_from_filename(video_file_path.name))
+
+    session_start_time = min(session_start_times)
 
     audio_folder_path = video_folder_path / "USVs"
     if audio_folder_path.exists():

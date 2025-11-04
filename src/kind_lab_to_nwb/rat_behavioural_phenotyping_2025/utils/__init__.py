eefa425b5a4370ab925d9516ddb4047f0b61f60a
Alessandra Trapani <alessandramaria.trapani@gmail.com>
2025-11-04 15:45:19 +0100

    Update import statements in utils to include convert_ffii_files_to_avi and dandi_ember_upload

--- src/kind_lab_to_nwb/rat_behavioural_phenotyping_2025/utils/__init__.py
+++ src/kind_lab_to_nwb/rat_behavioural_phenotyping_2025/utils/__init__.py
@@ -1,9 +1,10 @@
 from .utils import (
+    convert_ffii_files_to_avi,
     convert_ts_to_mp4,
+    dandi_ember_upload,
     extract_subject_metadata_from_excel,
     get_cage_ids_from_excel_files,
     get_session_ids_from_excel,
     get_subject_metadata_from_task,
     parse_datetime_from_filename,
-    dandi_ember_upload,
 )
